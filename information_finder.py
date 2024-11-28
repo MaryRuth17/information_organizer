b5ff21dad8ae89ccdc5b74d0b5830ec625031201
Mary Ruth <[maryruth.pesidas.relator@gmail.com]>
2024-11-28 22:13:39 +0800

    searching the name in the file

--- information_finder.py
+++ information_finder.py
@@ -1,11 +1,25 @@
-def main(): # find the name in the file
-    name = input("Enter the full name to search: ").strip()
+
+def main(): # asking the name to search in the file
+    name = input("Enter the full name to search for: ")
     result = search_name(name)
 
     if result:
-        print("Information Found:\n")
+        print("Information Found:")
         print(result)
 
     else:
         print(f"No information found for {name}.")
 
+def search_name(name): # seaarching the name in file
+    try:
+        with open("personal_info.txt", "r") as file:  # Open the file in read mode
+            for entry in file:
+                if name in entry: 
+                    return entry
+            
+    except :
+        print("The file does not exist.")
+        return None
+    
+if __name__ == "__main__":
+    main()
